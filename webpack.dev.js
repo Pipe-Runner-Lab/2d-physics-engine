d8db4f4c4986a0dcb388e08c9e1a4bd7df04547c
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-05-11 05:24:41 +0530

    WASM folder structure setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,47 +1,53 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
             options: {
-              configFile: "tsconfig.json",
-            },
-          },
+              configFile: 'tsconfig.json'
+            }
+          }
         ],
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
+        type: 'asset/resource'
+      }
+    ]
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js']
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "./public/index.html" },
-        { from: "./public/main.css" },
-        { from: "./public/favicon.ico" },
-      ],
+        { from: './public/index.html' },
+        { from: './public/main.css' },
+        { from: './public/favicon.ico' }
+      ]
+    }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, 'crate'),
+      outDir: path.resolve(__dirname, 'pkg'),
+      outName: "index"
     }),
   ],
 };
